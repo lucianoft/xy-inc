6fe3250208c83d47bccabe254614f2554e8c439e
fernandes <fernandes@DESKTOP-5QM21D0>
2017-08-27 22:03:40 -0300

    Primeiro Commit

---
+++ src/test/java/XyIncApplicationTests.java
@@ -0,0 +1,15 @@
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.web.WebAppConfiguration;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@WebAppConfiguration
+public class XyIncApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
